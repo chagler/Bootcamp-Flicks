2acd690602ba8836462276de3ce26c805de3591f
OverHead Display <chagler@oughtnotice-lm>
2017-01-23 10:33:38 -0800

    Initial Commit

---
+++ Flicks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flicks
+//
+//  Created by  Carol Hagler on 1/23/17.
+//  Copyright © 2017  Carol Hagler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
